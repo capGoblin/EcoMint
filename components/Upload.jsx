e18b750cfe0a7e59d5792d719eb30877934ada02
fayaz-rafin <fayaz.rafin@gmail.com>
2024-02-18 02:10:52 -0500

    added a new upload button from a mui component

--- components/Upload.jsx
+++ components/Upload.jsx
@@ -1,72 +1,29 @@
-//import material ui
-import React from 'react';
-import { Modal } from '@mui/material';
-import { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 
-export default function Upload(){
-    const [isBrowser, setIsBrowser] = useState(false);
-    let text = "";
-    
-    useEffect(() => {
-        setIsBrowser(true);
-    }, []);
-    
-    const handleClose = () => {
-        if (Modal.loading) return;
-    
-        setModal({ ...Modal, show: false });
-    };
-    
-    if (Modal.method === "Burn") text = "1 ECO Burned";
-    if (Modal.method === "Mint") text = "Mint Sucessful";
-    if (Modal.method === "Transfer") text = "Transfer Sucessful";
-    
-    const ModalContent = Modal.show ? (
-        <>
-        <div onClick={handleClose} className={styles.overlay} />
-        <div className={styles.container}>
-            {Modal?.loading ? (
-            <p>Transaction Initiated</p>
-            ) : (
-            <p>Transaction Mined</p>
-            )}
-            {Modal?.loading ? (
-            <Loader />
-            ) : (
-            <>
-                <BsFillCheckCircleFill className={styles.icon} />
-                <p>{text}</p>
-            </>
-            )}
-            <a
-            href={`https://mumbai.polygonscan.com/tx/${Modal.txn}`}
-            target="_blank"
-            rel="noreferrer"
-            className={styles.link}
-            >
-            View on Block Explorer <GoLinkExternal />
-            </a>
-            {!Modal?.loading && (
-                    <Button
-                        component="label"
-                        role={undefined}
-                        variant="contained"
-                        tabIndex={-1}
-                        startIcon={<CloudUploadIcon />} 
-                        onClick={handleClose} 
-                        className={styles.close}>
-                Close
-            </Button>
-            )}
-        </div>
-        </>
-    ) : null;
-    
-    return isBrowser
-        ? ReactDOM.createPortal(
-            ModalContent,
-            document.getElementById("modal-root")
-        )
-        : null;   
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+export default function InputFileUpload() {
+    return (
+        <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+        >
+            Upload file
+            <VisuallyHiddenInput type="file" />
+        </Button>
+    );
 }
